00a0b0a13a30dfd7fb0007c6599b81c53b979730
roni rimane <rimanroni386@gmail.comm>
2024-11-08 09:40:13 +0600

    problem 3 ok

--- main.js
+++ main.js
@@ -11,3 +11,14 @@ function removeDuplicates(input) {
     });
 }
 // console.log(removeDuplicates([2,2, 3, 3, 44, 44]));
+// Problem 3: 
+function countWordOccurrences(sentence, word) {
+    var sentenceLowerCase = sentence.toLowerCase();
+    var wordLowerCase = word.toLowerCase();
+    var sentenceSplit = sentenceLowerCase.split(' ');
+    var result = sentenceSplit.filter(function (currentWord) { return currentWord === wordLowerCase; }).length;
+    return result;
+}
+;
+// console.log(countWordOccurrences('hello riman roni riman bhai', 'riman'))
+// Problem 4: 
--- main.ts
+++ main.ts
@@ -22,4 +22,24 @@ function removeDuplicates(input:number[]):number[]{
 
 // console.log(removeDuplicates([2,2, 3, 3, 44, 44]));
 
+// Problem 3: 
+
+function countWordOccurrences(sentence:string, word:string):number{
+    const sentenceLowerCase = sentence.toLowerCase();
+    const wordLowerCase = word.toLowerCase();
+
+    const sentenceSplit = sentenceLowerCase.split(' ');
+    const result = sentenceSplit.filter(currentWord=>currentWord===wordLowerCase).length;
+    return result;
+
+};
+
+// console.log(countWordOccurrences('hello riman roni riman bhai', 'riman'))
+
+
+// Problem 4: 
+
+
+
+
 
